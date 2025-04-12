196bf5d6f018c11415a98d6a1d21b931f1195a5f
Gledyson <fgledyson5@gmail.com>
2025-04-12 20:49:05 -0300

    Layout do projeto

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, page, post
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', page, name='post'),
+    path('page/', post, name='page'),
 ]
 
